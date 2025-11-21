c91aabd84cd3823a2103038f2100c2299892b245
Anirudh Ryali <aryali97@gmail.com>
2025-11-20 20:24:58 -0500

    Initial Commit

---
+++ Packing List/Packing_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Packing_ListApp.swift
+//  Packing List
+//
+//  Created by Anirudh Ryali on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Packing_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
